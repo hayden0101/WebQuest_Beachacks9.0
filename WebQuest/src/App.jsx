f8eca6dcfe261b1f6d31a1fcf07a8b1e89c07934
andrebadrous <andre.badrous01@gmail.com>
2026-03-21 18:59:40 -0700

    start

--- WebQuest/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import MainPage from './components/MainPage';
-
-function App() {
-  return <div><MainPage /></div>
-}
-
-export default App;
--- webquest/src/Signup.jsx
+++
@@ -1,5 +0,0 @@
-function Signup() {
-  return <h1>Enter Username, Email, Password</h1>;
-}
-
-export default Signup;
