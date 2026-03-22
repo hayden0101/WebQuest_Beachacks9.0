5c83cee91f1227345ca12fbc1c4a160fe8add53d
Haydengc <haydenchelstrom@gmail.com>
2026-03-21 18:06:08 -0700

    Hello world login

---
+++ WebQuest/src/components/Login/login.jsx
@@ -0,0 +1,24 @@
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import Signup from "./Signup";
+// import Forgotpass from "./Forgotpass";
+
+function Login() {
+  return <h1>Hello</h1>;
+  // return (
+  //   <BrowserRouter>
+  //     {/* Navigation */}
+  //     <nav>
+  //       <Link to="/sign">Don't Have Account?</Link> |{" "}
+  //       <Link to="/forgot">Forgot Password?</Link>
+  //     </nav>
+
+  //     {/* Routes */}
+  //     <Routes>
+  //       <Route path="/sign" element={<Signup />} />
+  //       <Route path="/forgot" element={<Forgotpass />} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
+}
+
+export default Login;
