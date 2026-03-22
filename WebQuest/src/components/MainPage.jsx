887f86289ebdc8c80a6dc0d1d2aac937e49bf68f
andrebadrous <andre.badrous01@gmail.com>
2026-03-21 17:59:31 -0700

    hello world

---
+++ WebQuest/src/components/MainPage.jsx
@@ -0,0 +1,17 @@
+
+
+function MainPage() {
+    const dre = 'goat'
+    
+    return (
+        <>
+            <h1>WebQuest</h1>
+            <div>
+                <button>play as Guest</button>
+                <button onclick="window.location.href='">Login</button>
+            </div>
+        </>
+    )
+}
+
+export default MainPage;
